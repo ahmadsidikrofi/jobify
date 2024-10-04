206f4a265420fbb9400080dcebf32ebe90c9e255
ahmadsidikrofi <rofidragon71@gmail.com>
2024-10-04 16:55:19 +0700

    first commit

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,9 @@
+const AddJobPage = () => {
+    return ( 
+        <main>
+            <h1 className="text-4xl">Add Job Page</h1>
+        </main>
+    );
+}
+ 
+export default AddJobPage;
---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,9 @@
+const JobsPage = () => {
+    return ( 
+        <main>
+            <h1 className="text-4xl">Jobs Page</h1>
+        </main>
+     );
+}
+ 
+export default JobsPage;
